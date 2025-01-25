0602283e09e239a5439a0a3c37a81f7966ce3473
Holden Casey <me@holdencasey.com>
2025-01-24 21:43:12 -0500

    adds Task class

--- TaskScheduler/Program.cs
+++ TaskScheduler/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace TaskScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task myTask = new Task
+            {
+                Name = "My Task",
+                Description = "My Task description",
+                Duration = TimeSpan.FromMinutes(30),
+                StartTime = DateTime.Now,
+                IsRecurring = true,
+                RecurrenceInterval = TimeSpan.FromHours(1),
+            };
+            
+            Console.WriteLine(myTask);
+        }
+    }
+}
--- TaskScheduler/Task.cs
+++ TaskScheduler/Task.cs
@@ -2,5 +2,16 @@ namespace TaskScheduler;
 
 public class Task
 {
-    
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime StartTime { get; set; }
+    public TimeSpan Duration { get; set; }
+    public bool IsRecurring { get; set; }
+    public TimeSpan? RecurrenceInterval { get; set; }
+
+    public override string ToString()
+    {
+        return $"Task: {Name}, Starts at: {StartTime}, Duration: {Duration}, Recurring: {IsRecurring}";
+    }
 }
